99e5f17ce821aa96ddfe01929160099ddada832f
Elijah Charles <elij@lypy.com>
2017-01-07 05:53:38 +0000

    Addition of capture macro
    Captures macro result to aid type inference (alt, opt etc.)

--- src/parser.rs
+++ src/parser.rs
@@ -1,31 +1,28 @@
 use nom::multispace;
 
+/// Captures macro result to aid type inference (alt, opt etc.)
 #[macro_export]
-macro_rules! opt_alias(
+macro_rules! cap(
   ($i:expr, $submac:ident!( $($args:tt)* )) => (
-    {
-      let rs: ::nom::IResult<_, _> =
-      match $submac!($i, $($args)*) {
-        ::nom::IResult::Done(i,o)     => ::nom::IResult::Done(i, ::std::option::Option::Some(o)),
-        ::nom::IResult::Error(_)      => ::nom::IResult::Done($i, ::std::option::Option::None),
-        ::nom::IResult::Incomplete(i) => ::nom::IResult::Incomplete(i)
-      };
-      rs
-    }
+      {
+          let rs: ::nom::IResult<_, _> =
+              $submac!($i, $($args)* );
+          rs
+      }
   );
-  ($i:expr, $f:expr) => (
-    opt!($i, call!($f));
+  ($i:expr,$f:expr) => (
+    cap!($i, call!($f));
   );
 );
 
 named!(pub parse_field<&str, Option<&str> >,
-       do_parse!(tag!("|") >> x: opt_alias!(take_until_either!("|\r\n")) >> (x)));
+       do_parse!(tag!("|") >> x: cap!(opt!(cap!(alt!(is_not!("|\r\n"))))) >> (x)));
 named!(pub parse_fields<&str, Vec<Option<&str> > >, many1!(parse_field));
 named!(pub parse_segment<&str, (&str, Vec<Option<&str>>)>,
        do_parse!(
            name: is_not!("|") >>
            fields: parse_fields >>
-           opt_alias!(multispace) >>
+           cap!(opt!(multispace)) >>
                (name, fields)
        ));
 named!(pub parse_segments<&str, Vec<(&str, Vec<Option<&str>>)> >, many1!(parse_segment));
